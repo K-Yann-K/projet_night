aa731ff4e322b1094c5d98d9ac87e32b36288795
Yannick <yk.105m@gmail.com>
2024-12-06 05:48:12 +0100

    first commit

---
+++ div_question.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question:"",
+        answers: [
+           { text: "", correct: false},
+           { text: "", correct: true},
+           { text: "", correct: false},
+           { text: "", correct: false},
+        ]
+    },
+    {
+        question:"",
+        answers: [
+           { text: "", correct: false},
+           { text: "", correct: true},
+           { text: "", correct: false},
+           { text: "", correct: false},
+        ]
+    },
+    {
+        question:"",
+        answers: [
+           { text: "", correct: false},
+           { text: "", correct: true},
+           { text: "", correct: false},
+           { text: "", correct: false},
+        ]
+    },
+    {
+        question:"",
+        answers: [
+           { text: "", correct: false},
+           { text: "", correct: true},
+           { text: "", correct: false},
+           { text: "", correct: false},
+        ]
+    }
+];
